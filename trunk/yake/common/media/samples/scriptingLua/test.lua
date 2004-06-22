00bab1007ec673cea93fb0cc1d8d7970a8658cdb
codeandroid <codeandroid@21facc31-32d9-0310-a21e-8743ab810763>
2004-06-22 21:36:31 +0000

    added test of bindings

--- trunk/yake/common/media/samples/scriptingLua/test.lua
+++ trunk/yake/common/media/samples/scriptingLua/test.lua
@@ -2,3 +2,8 @@ print("from a file");
 a = 1;
 b = 3;
 print( a .. " + " .. b .. " = " .. a+b );
+print("creating player...\n");
+p = yake.Player();
+print("setting name to YAKE\n");
+p:setName('YAKE');
+print("reading name from player object... player name is " .. p:getName() );
