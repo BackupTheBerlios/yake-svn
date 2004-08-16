6540bd633bcdd92172bf2a9685c43958f453528f
codeandroid <codeandroid@21facc31-32d9-0310-a21e-8743ab810763>
2004-08-16 22:55:41 +0000

    added "flying" spot light, shadows, ...

--- trunk/yake/src/yake/samples/graphics/demo/yakeGraphicsDemo.cpp
+++ trunk/yake/src/yake/samples/graphics/demo/yakeGraphicsDemo.cpp
@@ -22,22 +22,6 @@ using namespace yake::base::math;
 using namespace yake::graphics;
 using namespace yake::data;
 
-void createDebugGeometryForComplexPhysical(
-			const physics::IComplexObject* pComplex,
-			graphics::IGraphicalWorld* pWorld,
-			SharedSceneNodeList & nodes,
-			SharedMUCList & ctrlrs)
-{
-	Vector<graphics::ISceneNode*> newNodes;
-	createDebugGeometryForComplexPhysical( pComplex, pWorld, newNodes, ctrlrs );
-	VectorIterator< Vector<graphics::ISceneNode*> > it( newNodes.begin(), newNodes.end() );
-	while (it.hasMoreElements())
-	{
-		graphics::ISceneNode* pSN = it.getNext();
-		nodes.push_back( SharedPtr<graphics::ISceneNode>( pSN ) );
-	}
-}
-
 class TheApp : public yake::exapp::ExampleApplication
 {
 private:
@@ -45,6 +29,7 @@ private:
 	SharedPtr< IGraphicalWorld >			mGWorld;
 
 	graphics::ISceneNode*				mLightOneNode;
+	graphics::ISceneNode*				mLightTwoNode;
 
 	struct SimpleOne {
 		graphics::ISceneNode*			pSN;
@@ -52,6 +37,10 @@ private:
 	};
 	SimpleOne							mNinja;
 	SimpleOne							mGround;
+	SharedPtr<graphics::ILight>			mSunLight;
+	SharedPtr<graphics::ISceneNode>		mSunLightNode;
+
+	SimpleSpline						mCamPath;
 public:
 	TheApp() : ExampleApplication( true /*graphics*/,
 									false /*physics*/,
@@ -59,7 +48,8 @@ public:
 									true /*input*/,
 									false /*script bindings*/,
 									false /*audio*/),
-				mLightOneNode(0)
+				mLightOneNode(0),
+				mLightTwoNode(0)
 	{
 		mNinja.pSN = 0;
 		mGround.pSN = 0;
@@ -100,12 +90,12 @@ public:
 		mNinja.pSN = mGWorld->createSceneNode();
 		YAKE_ASSERT( mNinja.pSN );
 
-		mNinja.pE = mGWorld->createEntity("ninja.mesh");
+		mNinja.pE = mGWorld->createEntity("fordboss.mesh"); //"ninja.mesh");
 		YAKE_ASSERT( mNinja.pE );
 		mNinja.pE->setCastsShadow( true );
 
 		mNinja.pSN->attachEntity( mNinja.pE );
-		mNinja.pSN->setPosition( Vector3(0,0,-500) );
+		mNinja.pSN->setPosition( Vector3(0,0,-200) );
 	}
 
 	void setupGround()
@@ -119,23 +109,132 @@ public:
 		mGround.pSN = mGWorld->createSceneNode();
 		YAKE_ASSERT( mGround.pSN );
 		mGround.pSN->attachEntity( mGround.pE );
-		mGround.pSN->setScale( Vector3(100,100,100) );
+		mGround.pSN->setScale( Vector3(100,1,100) );
 	}
 
 	void setupLights()
 	{
-		if (mLightOneNode)
-			return;
+		// fixed light (sun)
+		mSunLight.reset( mGWorld->createLight() );
+		mSunLightNode.reset( mGWorld->createSceneNode() );
+		mSunLightNode->attachLight( mSunLight.get() );
+
+		//mSunLight->setType(ILight::LT_SPOT);
+		mSunLight->setType(ILight::LT_DIRECTIONAL);
+        mSunLightNode->setPosition(Vector3(-1000,1250,500));
+        Vector3 dir;
+        dir = -mSunLightNode->getPosition();
+        dir.normalise();
+        mSunLight->setDirection(dir);
+        mSunLight->setDiffuseColour(Color(0.35, 0.35, 0.38));
+        mSunLight->setSpecularColour(Color(0.9, 0.9, 1));
+		mSunLight->setCastsShadow( true );
+		//mSunLight->setSpotlightRange(80,90,1);
+
+		// movable light 1
+		YAKE_ASSERT(0==mLightOneNode);
 		mLightOneNode = mGWorld->createSceneNode();
 		YAKE_ASSERT( mLightOneNode );
 		graphics::ILight* pL = mGWorld->createLight();
 		YAKE_ASSERT( pL );
 		mLightOneNode->attachLight( pL );
-		pL->setType( graphics::ILight::LT_POINT );
-		pL->setCastsShadow( false  );
-		pL->setDiffuseColour( Color(1,1,1) );
-		pL->setAttenuation( 8000, 1, 0.0005, 0 );
+		pL->setDiffuseColour( Color(0.6,0.7,0.8) );
 		pL->setSpecularColour( Color(1,1,1) );
+		pL->setAttenuation( 8000, 1, 0.0005, 0 );
+
+//		pL->setType( graphics::ILight::LT_POINT );
+		pL->setType( graphics::ILight::LT_SPOT );
+		pL->setDirection( Vector3(0,0,1) );
+		pL->setSpotlightRange(80,100,1);
+		pL->setCastsShadow( true );
+
+        mLightOneNode->setPosition(Vector3(300,250,-300));
+
+		// movable light 2
+		YAKE_ASSERT(0==mLightTwoNode);
+		mLightTwoNode = mGWorld->createSceneNode();
+		YAKE_ASSERT( mLightTwoNode );
+		pL = mGWorld->createLight();
+		YAKE_ASSERT( pL );
+		mLightTwoNode->attachLight( pL );
+		pL->setDiffuseColour( Color(0.6,0.7,0.8) );
+		pL->setSpecularColour( Color(1,1,1) );
+		pL->setAttenuation( 8000, 1, 0.0005, 0 );
+
+		pL->setType( graphics::ILight::LT_SPOT );
+		Vector3 n(0,-1,-1);
+		n.normalise();
+		pL->setDirection( n );
+		pL->setSpotlightRange(80,100,1);
+		pL->setCastsShadow( true );
+
+        mLightTwoNode->setPosition(Vector3(0,150,0));
+	}
+
+	void setupScene()
+	{
+		// cube
+		graphics::ISceneNode* node = 0;
+        node = mGWorld->createSceneNode();
+		YAKE_ASSERT( node );
+		graphics::IEntity* ent = 0;
+        ent = mGWorld->createEntity( "cube.mesh" );
+		YAKE_ASSERT( ent );
+		ent->setCastsShadow(true);
+		ent->setMaterial("Examples/Athene/Basic");
+        node->attachEntity( ent );
+        node->translate(Vector3(0,-20, 0));
+        //node->yaw(90);
+
+		// Floor plane
+		ent = mGWorld->createEntity("plane_1x1.mesh");
+		YAKE_ASSERT( ent );
+		node = mGWorld->createSceneNode();
+		YAKE_ASSERT( node );
+		node->attachEntity( ent );
+		node->setScale( Vector3(1000,1,1000) );
+		ent->setMaterial("Examples/BumpyMetal");
+		ent->setCastsShadow(false);
+
+		// column
+		ent = mGWorld->createEntity("column.mesh");
+		YAKE_ASSERT( ent );
+		node = mGWorld->createSceneNode();
+		YAKE_ASSERT( node );
+		node->attachEntity( ent );
+		ent->setMaterial("Examples/Athene/Basic");
+		ent->setCastsShadow(true);
+		node->translate(Vector3(200,100, -200));
+
+		// column
+		ent = mGWorld->createEntity("column.mesh");
+		YAKE_ASSERT( ent );
+		node = mGWorld->createSceneNode();
+		YAKE_ASSERT( node );
+		node->attachEntity( ent );
+		ent->setMaterial("Examples/Rockwall");
+		ent->setCastsShadow(true);
+		node->translate(Vector3(-200,0, -200));
+
+		// column
+		ent = mGWorld->createEntity("column.mesh");
+		YAKE_ASSERT( ent );
+		node = mGWorld->createSceneNode();
+		YAKE_ASSERT( node );
+		node->attachEntity( ent );
+		ent->setMaterial("Examples/Rockwall");
+		ent->setCastsShadow(true);
+		node->translate(Vector3(-200,0, 200));
+
+		// column
+		ent = mGWorld->createEntity("column.mesh");
+		YAKE_ASSERT( ent );
+		node = mGWorld->createSceneNode();
+		YAKE_ASSERT( node );
+		node->attachEntity( ent );
+		ent->setMaterial("Examples/Rockwall");
+		ent->setCastsShadow(true);
+		node->translate(Vector3(200,0, 200));
 	}
 
 	virtual void run()
@@ -158,8 +257,8 @@ public:
 
 		if (mVPs[0].second)
 		{
-			mVPs[0].second->translate( Vector3(0,1.6,20) );
-			mVPs[0].second->pitch(-30);
+			mVPs[0].second->translate( Vector3(0,100,700) );
+			mVPs[0].second->pitch(-10);
 		}
 		if (mVPs.size() > 1 && mVPs[1].second)
 			mVPs[1].second->setPosition( Vector3(0,2,-80) );
@@ -174,10 +273,19 @@ public:
 			mVPs[3].second->pitch(-90);
 		}
 
+		//
+		mCamPath.addPoint( Vector3(-400,150,-400) );
+		mCamPath.addPoint( Vector3( 300,150,-400) );
+		mCamPath.addPoint( Vector3( 300,150, 400) );
+		mCamPath.addPoint( Vector3(-400,150, 400) );
+		mCamPath.addPoint( Vector3(-400,150,-400) );
+		mCamPath.recalcTangents();
+
 		// objects
 		setupLights();
 		setupNinja();
 		setupGround();
+		setupScene();
 
 		// main loop
 		real lastTime = base::native::getTime();
@@ -189,7 +297,6 @@ public:
 			lastTime = time;
 
 			// handle input
-			PROFILE_BEGIN( input );
 			getInputSystem().update();
 			mMouseEventGenerator.update();
 			mKeyboardEventGenerator.update();
@@ -199,7 +306,7 @@ public:
 			if (getKeyboard())
 			{
 				static iCam = 0;
-				real distance = -5. * timeElapsed;
+				real distance = -200. * timeElapsed;
 				if (getKeyboard()->isKeyDown(input::KC_LEFT))
 					mVPs[iCam].second->translate( Vector3(distance, 0, 0) );
 				if (getKeyboard()->isKeyDown(input::KC_RIGHT))
@@ -208,15 +315,49 @@ public:
 					mVPs[iCam].second->translate( Vector3(0, 0, distance) );
 				if (getKeyboard()->isKeyDown(input::KC_DOWN))
 					mVPs[iCam].second->translate( Vector3(0, 0, -distance) );
+				if (getKeyboard()->isKeyDown(input::KC_A))
+					mNinja.pSN->translate( Vector3(distance, 0, 0) );
+				if (getKeyboard()->isKeyDown(input::KC_D))
+					mNinja.pSN->translate( Vector3(-distance, 0, 0) );
 			}
 
-			// update visual box representation from the physical one
-			mLightOneNode->setPosition(
-				mNinja->pSN->getPosition() + Vector3(0,10,0) );
+			static real p1 = 0;
+			p1 += timeElapsed * 0.05;
+			const real max = 1. - (1. / mCamPath.getNumPoints());
+			while (p1 > max)
+				p1 -= max;
+			mLightOneNode->setPosition( mCamPath.interpolate(p1) );
+			mLightOneNode->setFixedYawEnabled( true );
+			mLightOneNode->setFixedYawAxis( Vector3(0,1,0) );
+			mLightOneNode->lookAt( Vector3(0,0,0) );
+
+			std::cout << p1 << " | " << mLightOneNode->getPosition().x << ", " << mLightOneNode->getPosition().y << ", " << mLightOneNode->getPosition().z << "\n";
+
+			/*static real l1v = 200;
+			Vector3 v(l1v * timeElapsed, 0, 0);
+			mLightOneNode->translate( v );
+			if (fabs(mLightOneNode->getPosition().x) > 800)
+			{
+				l1v *= -1;
+				mLightOneNode->translate( -v );
+			}*/
+			static real sunv = 100;
+			Vector3 v(0, 0, sunv * timeElapsed);
+			mSunLightNode->translate( v );
+			if (fabs(mSunLightNode->getPosition().z) > 800)
+			{
+				sunv *= -1;
+				mSunLightNode->translate( -v );
+			}
+
+			mLightTwoNode->setPosition( mVPs[0].second->getPosition() + Vector3(100,100,0) );
 
 			// render the scene
 			mGWorld->render( timeElapsed );
 		}
+
+		mSunLightNode.reset();
+		mSunLight.reset();
 
 		YAKE_SAFE_DELETE( mLightOneNode );
 
