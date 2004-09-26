b10b6c128c13866b289760e96522bb671094257f
codeandroid <codeandroid@21facc31-32d9-0310-a21e-8743ab810763>
2004-09-26 19:12:18 +0000

    initial revision

---
+++ trunk/yake/yapp/model/yakeGraphical.h
@@ -0,0 +1,59 @@
+/**------------------------------------------------------------------------------------
+   This file is part of YAKE
+   Copyright © 2004 The YAKE Team
+   For the latest information visit http://www.yake.org 
+   ------------------------------------------------------------------------------------
+   This program is free software; you can redistribute it and/or modify it under
+   the terms of the GNU Lesser General Public License as published by the Free Software
+   Foundation; either version 2 of the License, or (at your option) any later
+   version.
+
+   This program is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+   FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public License along with
+   this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+   Place - Suite 330, Boston, MA 02111-1307, USA, or go to
+   http://www.gnu.org/copyleft/lesser.txt.
+   ------------------------------------------------------------------------------------
+   If you are interested in another license model contact the Yake Team via
+   E-Mail: team@yake.org.
+   For more information see the LICENSE file in the root directory of the
+   source code distribution.
+   ------------------------------------------------------------------------------------
+*/
+#ifndef YAPP_MODEL_GRAPHICAL_H
+#define YAPP_MODEL_GRAPHICAL_H
+
+#include <yapp/base/yappPrerequisites.h>
+#include <yake/graphics/yakeGraphicsSystem.h>
+
+
+namespace yake {
+	using namespace base::templates;
+namespace app {
+namespace model {
+
+	/** A graphical container model.
+	*/
+	class YAPP_BASE_API Graphical
+	{
+	public:
+		Graphical()
+		{}
+		~Graphical();
+		typedef Vector<graphics::ISceneNode*> SceneNodeList;
+		void addSceneNode( graphics::ISceneNode* pSceneNode, bool bTransferOwnership = true );
+		SceneNodeList getRootSceneNodes() const;
+		void fromDotScene(const String & fn, graphics::IGraphicalWorld* pGWorld);
+	private:
+		typedef AssocVector< graphics::ISceneNode*, bool > NodeMap;
+		NodeMap		mNodes;
+	};
+
+} // model
+} // app
+} // yake
+
+#endif
