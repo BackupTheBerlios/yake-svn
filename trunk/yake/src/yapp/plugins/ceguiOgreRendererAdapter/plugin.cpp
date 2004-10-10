2114a3239eda24383073c4d78105a5377f142b9c
codeandroid <codeandroid@21facc31-32d9-0310-a21e-8743ab810763>
2004-10-10 23:32:41 +0000

    initial revision

---
+++ trunk/yake/src/yapp/plugins/ceguiOgreRendererAdapter/plugin.cpp
@@ -0,0 +1,84 @@
+/**------------------------------------------------------------------------------------
+   This file is part of YAKE
+   Copyright © 2004 The YAKE Team
+   For the latest information visit http://www.yake.org 
+   ------------------------------------------------------------------------------------
+   This program is free software; you can redistribute it and/or modify it under
+   the terms of the GNU Lesser General Public License as published by the Free Software
+   Foundation; either version 2 of the License, or (at your option) any later
+   version.
+
+   This program is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+   FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public License along with
+   this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+   Place - Suite 330, Boston, MA 02111-1307, USA, or go to
+   http://www.gnu.org/copyleft/lesser.txt.
+   ------------------------------------------------------------------------------------
+*/
+#include <yapp/plugins/ceguiOgreRendererAdapter/pch.h>
+#include "CEGUI.h"
+#include <dependencies/ogrenew/OgreMain/include/Ogre.h>
+#include "renderers/OgreGUIRenderer/ogrerenderer.h"
+#include <yapp/common/yakeCEGUIRendererAdapter.h>
+#include <yapp/plugins/ceguiOgreRendererAdapter/plugin.h>
+
+
+//---------------------------------------------------------
+// This is an example of an exported function.
+yake::base::Plugin* dynlibStartPlugin(void)
+{
+	return new ThePlugin();
+}
+
+//---------------------------------------------------------
+ThePlugin::ThePlugin()
+{
+}
+
+//---------------------------------------------------------
+ThePlugin::~ThePlugin()
+{
+}
+
+//---------------------------------------------------------
+yake::base::String ThePlugin::getName() const
+{
+	return yake::base::String("yake.misc.ceguiadapter");
+}
+
+//---------------------------------------------------------
+yake::base::Version ThePlugin::getVersion() const
+{
+	return yake::base::Version( 0, 1, 0 );
+}
+
+//---------------------------------------------------------
+bool ThePlugin::initialise()
+{
+	Ogre::RenderWindow* pWin = Ogre::Root::getSingleton().getAutoCreatedWindow();
+	YAKE_ASSERT( pWin ).error("Need a valid render window!");
+	mGUIRenderer = new CEGUI::OgreRenderer(
+		pWin, 
+		Ogre::RENDER_QUEUE_OVERLAY, 
+		true, 
+		3000,
+		Ogre::ST_GENERIC );
+		//Ogre::Root::getSingleton()._getCurrentSceneManager());
+	YAKE_ASSERT( mGUIRenderer );
+	return (mGUIRenderer != 0);
+}
+
+//---------------------------------------------------------
+bool ThePlugin::shutdown()
+{
+	YAKE_SAFE_DELETE(mGUIRenderer);
+	return true;
+}
+//---------------------------------------------------------
+CEGUI::Renderer* ThePlugin::getRenderer()
+{
+	return mGUIRenderer;
+}
