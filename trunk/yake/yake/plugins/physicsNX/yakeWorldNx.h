7c403c06e8b0a5ff099159342ee4550e40a77d81
codeandroid <codeandroid@21facc31-32d9-0310-a21e-8743ab810763>
2004-11-30 18:25:52 +0000

    added license, added methods, added "step" signal

--- trunk/yake/yake/plugins/physicsNX/yakeWorldNx.h
+++ trunk/yake/yake/plugins/physicsNX/yakeWorldNx.h
@@ -1,3 +1,23 @@
+/**------------------------------------------------------------------------------------
+   This file is part of YAKE
+   Copyright © 2004 The YAKE Team
+   For the latest information visit http://www.yake.org 
+   ------------------------------------------------------------------------------------
+   This program is free software; you can redistribute it and/or modify it under
+   the terms of the GNU Lesser General Public License as published by the Free Software
+   Foundation; either version 2 of the License, or (at your option) any later
+   version.
+
+   This program is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+   FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public License along with
+   this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+   Place - Suite 330, Boston, MA 02111-1307, USA, or go to
+   http://www.gnu.org/copyleft/lesser.txt.
+   ------------------------------------------------------------------------------------
+*/
 #ifndef YAKE_NX_WORLD_H
 #define YAKE_NX_WORLD_H
 
@@ -12,6 +32,10 @@ namespace physics {
 
 		virtual SharedPtr<IJoint> createJoint( const IJoint::DescBase & rkJointDesc );
 		virtual SharedPtr<IActor> createActor( const IActor::Desc & rkActorDesc );
+		virtual SharedPtr<IAvatar> createAvatar( const IAvatar::Desc & rkAvatarDesc );
+		virtual SharedPtr<IMaterial> createMaterial( const IMaterial::Desc & rkMatDesc );
+
+		virtual TriangleMeshId createTriangleMesh( const TriangleMeshDesc & rkTrimeshDesc );
 
 		virtual Deque<ShapeType> getSupportedShapes(bool bStatic = true, bool bDynamic = true) const;
 		virtual Deque<JointType> getSupportedJoints() const;
@@ -23,7 +47,12 @@ namespace physics {
 
 		virtual void step(const real timeElapsed);
 
+		//-- helpers
 		bool init_();
+
+		typedef Signal1<void(const real)> StepSignal;
+		StepSignal	stepSignal;
+
 	private:
 		String		mCurrentSolver;
 		NxScene*	mpScene;
