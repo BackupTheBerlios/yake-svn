e62304a002a220e5dcf2d8589a55d4c56b121fff
codeandroid <codeandroid@21facc31-32d9-0310-a21e-8743ab810763>
2004-12-08 21:53:40 +0000

    major update: simple objects like boxes and more...

--- trunk/yake/src/yake/samples/physics/demo/yakeDemo.cpp
+++ trunk/yake/src/yake/samples/physics/demo/yakeDemo.cpp
@@ -17,10 +17,22 @@ public:
 	void cleanUp();
 private:
 	// types
-	typedef std::pair< SharedPtr<graphics::IViewport>, SharedPtr<graphics::ICamera> > ViewportCameraPair;
+	struct View
+	{
+		graphics::IViewport*	pViewport;
+		graphics::ICamera*		pCamera;
+
+		View(graphics::ICamera* pC, graphics::IViewport* pV) : pViewport(pV), pCamera(pC)
+		{}
+	};
+	//typedef std::pair< SharedPtr<graphics::IViewport>, SharedPtr<graphics::ICamera> > ViewportCameraPair;
 	typedef Deque< SharedPtr<graphics::ISceneNode> > SceneNodes;
 	typedef Deque< SharedPtr<graphics::IEntity> > Entities;
 	typedef Deque< SharedPtr<graphics::ILight> > Lights;
+	//typedef Deque< SharedPtr<ViewportCameraPair> > Views;
+	typedef Deque< SharedPtr<View> > Views;
+
+	SharedPtr< physics::IMaterial >	mBouncyPhysicsMaterial;
 
 	struct Simple
 	{
@@ -32,12 +44,14 @@ private:
 			YAKE_ASSERT( pSN.get() );
 			YAKE_ASSERT( pActor.get() );
 			pSN->setPosition( pActor->getPosition() );
+			pSN->setOrientation( pActor->getOrientation() );
 		}
 	};
 private:
 	// variables
 	bool						mShutdownRequested;
-	ViewportCameraPair			mVpCamPair;
+
+	Views						mViews;;
 	SceneNodes					mSceneNodes;
 	Entities					mEntities;
 	Lights						mLights;
@@ -62,27 +76,60 @@ private:
 		if (itFind != mEntities.end())
 			mEntities.erase( itFind );
 	}
-	bool createBall( Simple & rSimple )
+	void createSimpleGraphicalObject(	Simple & rSimple,
+										const String & rkMesh,
+										const Vector3 & rkPosition = Vector3::kZero,
+										const Vector3 & rkScale = Vector3(1,1,1))
 	{
-		// graphics
 		rSimple.pSN.reset( mGWorld->createSceneNode() );
-		rSimple.pE.reset( mGWorld->createEntity("sphere_d1.mesh") );
+		rSimple.pE.reset( mGWorld->createEntity(rkMesh) );
+		rSimple.pE->setMaterial("Examples/BumpyMetal");
 		rSimple.pSN->attachEntity( rSimple.pE.get() );
-		rSimple.pSN->setScale(Vector3(2,2,2));
-
-		// physics
+		rSimple.pSN->setPosition( rkPosition );
+		rSimple.pSN->setScale( rkScale );
+	}
+	void createSimpleActor(	Simple & rSimple, 
+							physics::IShape::Desc & rkDesc,
+							const Vector3 & rkPosition,
+							const real mass = 1 )
+	{
 		rSimple.pActor = mPWorld->createActor();
 		YAKE_ASSERT( rSimple.pActor );
-		rSimple.pActor->createShape( physics::IShape::SphereDesc(real(2)) );
+		rSimple.pActor->createShape( rkDesc );
 		YAKE_ASSERT( rSimple.pActor->getShapes().size() == 1 );
 
 		rSimple.pActor->createBody();
 		YAKE_ASSERT( rSimple.pActor->hasBody() );
-		rSimple.pActor->getBody()->setMass( 5 );
-		rSimple.pActor->setPosition( Vector3(0,40,0) );
-		rSimple.pActor->getBody()->setLinearVelocity( Vector3(1,0,0) );
+		rSimple.pActor->getBody()->setMass( mass );
+		rSimple.pActor->setPosition( rkPosition );
+	}
+	bool createBall( Simple & rSimple, const Vector3 & rkPosition, const real radius )
+	{
+		// graphics
+		createSimpleGraphicalObject( rSimple, "sphere_d1.mesh", Vector3(0,0,0), radius*Vector3(2,2,2) );
+
+		// physics
+		createSimpleActor( rSimple, physics::IShape::SphereDesc(real(radius), mBouncyPhysicsMaterial.get()), rkPosition );
 
 		return true;
+	}
+	bool createBox( Simple & rSimple, const Vector3 & rkPosition, const Vector3 & rkDimensions )
+	{
+		// graphics
+		createSimpleGraphicalObject( rSimple, "box_1x1x1.mesh", Vector3(0,0,0), 2*rkDimensions );
+
+		// physics
+		createSimpleActor( rSimple, physics::IShape::BoxDesc(rkDimensions, mBouncyPhysicsMaterial.get()), rkPosition );
+
+		return true;
+	}
+	SharedPtr<physics::IJoint> createFixedJoint(Simple & rSimple1, Simple & rSimple2)
+	{
+		YAKE_ASSERT( rSimple1.pActor );
+		YAKE_ASSERT( rSimple2.pActor );
+		SharedPtr<physics::IJoint> pJoint = mPWorld->createJoint(
+			physics::IJoint::DescFixed( *rSimple1.pActor.get(), *rSimple2.pActor.get() ) );
+		return pJoint;
 	}
 	SharedPtr<base::Library> loadLib( const base::String & file )
 	{
@@ -130,7 +177,7 @@ private:
 	void createGroundPlane()
 	{
 		createSimpleEntity("plane_1x1.mesh");
-		mSceneNodes.back()->setScale(Vector3(50,1,50));
+		mSceneNodes.back()->setScale(Vector3(70,1,70));
 		mEntities.back()->setMaterial("Examples/BumpyMetal");
 		mEntities.back()->setCastsShadow(false);
 	}
@@ -158,50 +205,6 @@ private:
         pL->setDiffuseColour(Color(0.35, 0.35, 0.38));
         pL->setSpecularColour(Color(0.9, 0.9, 1));
 		pL->setCastsShadow( true );
-
-		// movable light 1
-		/*
-		pNode.reset( mGWorld->createSceneNode() );
-		YAKE_ASSERT( pNode );
-		mSceneNodes.push_back(pNode);
-
-		pL.reset( mGWorld->createLight() );
-		YAKE_ASSERT( pL );
-		mLights.push_back( pL );
-		pNode->attachLight( pL.get() );
-		pL->setDiffuseColour( Color(0.6,0.7,0.8) );
-		pL->setSpecularColour( Color(1,1,1) );
-		pL->setAttenuation( 8000, 1, 0.0005, 0 );
-
-//		pL->setType( graphics::ILight::LT_POINT );
-		pL->setType( graphics::ILight::LT_SPOT );
-		pL->setDirection( Vector3(0,0,1) );
-		pL->setSpotlightRange(80,100,1);
-		pL->setCastsShadow( true );
-
-        pNode->setPosition(Vector3(300,250,-300));
-
-		// movable light 2
-		pNode.reset( mGWorld->createSceneNode() );
-		YAKE_ASSERT( pNode );
-		mSceneNodes.push_back(pNode);
-		pL.reset( mGWorld->createLight() );
-		YAKE_ASSERT( pL );
-		mLights.push_back( pL );
-		pNode->attachLight( pL.get() );
-		pL->setDiffuseColour( Color(0.6,0.7,0.8) );
-		pL->setSpecularColour( Color(1,1,1) );
-		pL->setAttenuation( 8000, 1, 0.0005, 0 );
-
-		pL->setType( graphics::ILight::LT_SPOT );
-		Vector3 n(0,-1,-1);
-		n.normalise();
-		pL->setDirection( n );
-		pL->setSpotlightRange(80,100,1);
-		pL->setCastsShadow( true );
-
-		pNode->setPosition(Vector3(0,150,0));
-		*/
 	}
 };
 void MiniApp::init()
@@ -230,27 +233,85 @@ void MiniApp::init()
 	mGWorld = mGraphics->createWorld();
 	YAKE_ASSERT( mGWorld ).debug("Cannot create graphics world.");
 
-	mGWorld->setShadowsEnabled( false );
+	mGWorld->setShadowsEnabled( true );
 
 	mGraphics->subscribeToShutdownSignal( boost::bind(MiniApp::requestShutdown,this) );
 
-	mVpCamPair.second.reset( mGWorld->createCamera() );
-	mVpCamPair.second->setNearClipDistance( 1. );
-	mVpCamPair.second->setFarClipDistance( 2000. );
-	mVpCamPair.second->setPosition( Vector3(100,10,100) );
-	mVpCamPair.second->lookAt( Vector3(0,2,0) );
-	mVpCamPair.first.reset( mGWorld->createViewport( mVpCamPair.second.get() ) );
-	mVpCamPair.first->setDimensions(0,0,1,1);
-	mVpCamPair.first->setZ( 100 );
+	// main 3d view
+	{
+		/*
+	SharedPtr<ViewportCameraPair> vpPair( new ViewportCameraPair );
+	vpPair->second.reset( mGWorld->createCamera() );
+	vpPair->second->setNearClipDistance( 1. );
+	vpPair->second->setFarClipDistance( 2000. );
+	vpPair->second->setPosition( Vector3(50,50,50) );
+	vpPair->second->lookAt( Vector3(0,2,0) );
+	vpPair->first.reset( mGWorld->createViewport( vpPair->second.get() ) );
+	vpPair->first->setDimensions(0,0,1,1);
+	vpPair->first->setZ( 100 );
+	mViews.push_back( vpPair );*/
+	}
+
+	//
+	{
+//#define VP_1
+#ifdef VP_1
+		graphics::ICamera* pC = mGWorld->createCamera();
+		pC->setNearClipDistance( 1 );
+		pC->setFarClipDistance( 1000 );
+		pC->setPosition( Vector3(0,4,50) );
+		pC->lookAt( Vector3(0,1,0) );
+		graphics::IViewport* pV = mGWorld->createViewport( pC );
+		pV->setDimensions( 0, 0, 0.3, 0.3 );
+		pV->setZ( 101 );
+
+		mViews.push_back( SharedPtr<View>( new View(pC, pV) ) );
+#endif
+	/*
+	SharedPtr<ViewportCameraPair> vpPair( new ViewportCameraPair );
+	vpPair->second.reset( mGWorld->createCamera() );
+	vpPair->second->setNearClipDistance( 1. );
+	vpPair->second->setFarClipDistance( 2000. );
+	vpPair->second->setPosition( Vector3(50,50,50) );
+	vpPair->second->lookAt( Vector3(0,2,0) );
+	vpPair->first.reset( mGWorld->createViewport( vpPair->second.get() ) );
+	vpPair->first->setDimensions(0,0,0.2,0.2);
+	vpPair->first->setZ( 110 );
+	mViews.push_back( vpPair );
+	*/
+	}
+	{
+#define VP_2
+#ifdef VP_2
+		graphics::ICamera* pC = mGWorld->createCamera();
+		pC->setNearClipDistance( 1 );
+		pC->setFarClipDistance( 1000 );
+		pC->setPosition( Vector3(50,50,50) );
+		pC->lookAt( Vector3(0,1,0) );
+		graphics::IViewport* pV = mGWorld->createViewport( pC );
+		pV->setDimensions( 0, 0, 1, 1 );
+		pV->setZ( 100 );
+
+		mViews.push_back( SharedPtr<View>( new View(pC, pV) ) );
+#endif
+	}
 }
 void MiniApp::cleanUp()
 {
 	mLights.clear();
 	mEntities.clear();
 	mSceneNodes.clear();
+	mBouncyPhysicsMaterial.reset();
 	//
-	mVpCamPair.first.reset();
-	mVpCamPair.second.reset();
+	DequeIterator<Views> it(mViews.begin(), mViews.end());
+	while (it.hasMoreElements())
+	{
+		SharedPtr<View> & pView = it.getNext();
+		YAKE_ASSERT( pView );
+		YAKE_SAFE_DELETE( pView->pViewport );
+		YAKE_SAFE_DELETE( pView->pCamera );
+	}
+	mViews.clear();
 	// destroy worlds before we destroy the underlying system plugins
 	mPWorld.reset();
 	mGWorld.reset();
@@ -272,33 +333,90 @@ void MiniApp::run()
 {
 	using namespace physics;
 	YAKE_ASSERT( mPWorld );
+	//
+	mBouncyPhysicsMaterial = mPWorld->createMaterial(
+									IMaterial::Desc( 0.1, 1.5 )
+								);
+	YAKE_ASSERT( mBouncyPhysicsMaterial );
 
+	// static environment
 	SharedPtr<IActor> pStaticWorldActor = mPWorld->createActor();
 	YAKE_ASSERT( pStaticWorldActor );
+//#define TRI
+#ifdef TRI
+	/*
+	  3-2
+	  |/|
+	  0-1
+	*/
+	TriangleMeshDesc groundMeshDesc;
+	groundMeshDesc.vertices.push_back( Vector3(-100,0, 100) );
+	groundMeshDesc.vertices.push_back( Vector3( 100,0, 100) );
+	groundMeshDesc.vertices.push_back( Vector3( 100,0,-100) );
+	groundMeshDesc.vertices.push_back( Vector3(-100,0,-100) );
+	groundMeshDesc.indices.push_back( 0 ); // tri #1
+	groundMeshDesc.indices.push_back( 1 );
+	groundMeshDesc.indices.push_back( 2 );
+	groundMeshDesc.indices.push_back( 0 ); // tri #2
+	groundMeshDesc.indices.push_back( 2 );
+	groundMeshDesc.indices.push_back( 3 );
+	TriangleMeshId groundMeshId = mPWorld->createTriangleMesh(groundMeshDesc);
+	YAKE_ASSERT( kTriangleMeshIdNone != groundMeshId );
 
+	pStaticWorldActor->createShape( IShape::TriMeshDesc( groundMeshId ) );
+#else
 	pStaticWorldActor->createShape( IShape::PlaneDesc( Vector3(0,1,0), 0 ) );
+#endif
 	YAKE_ASSERT( pStaticWorldActor->getShapes().size() == 1 )(pStaticWorldActor->getShapes().size());
 
-	Simple s;
-	createBall( s );
+	// two very simple entities (if we dare call them that...)
+	Simple s,ball2;
+	createBall( s, Vector3(10,40,0), 1 );
+	createBox( ball2, Vector3(10,20,0), Vector3(5,5,2) );
 
-	createSimpleEntity("sphere_d1.mesh");
+#define TEST_FIXED_JOINT
+#ifdef TEST_FIXED_JOINT
+	SharedPtr<physics::IJoint> pJ = createFixedJoint(s, ball2);
+#endif
+
+	// create a line up of boxes
+	const size_t kNumBoxes = 12;
+	Simple boxes[kNumBoxes];
+	const real kBoxZ = 2;
+	for (size_t i=0; i<kNumBoxes; ++i)
+	{
+		createBox( boxes[i], Vector3(-10,1,-kBoxZ*4*i), Vector3(4,8,kBoxZ) );
+	}
+
+#ifndef TEST_FIXED_JOINT
+	// push the sphere against the line up of boxes
+	s.pActor->setPosition( Vector3(-11,2,2) );
+	s.pActor->getBody()->setMass( 30 );
+	s.pActor->getBody()->setLinearVelocity( Vector3(0,1,-20) );
+#endif
+
+	// eye candy :P
 	createGroundPlane();
-	createNinja();
+	//createNinja();
 	setupLights();
 
 	real lastTime = base::native::getTime();
 	while (!mShutdownRequested)
 	{
+		static uint32 siFrame = 0;
 		// timing
 		real time = base::native::getTime();
 		real timeElapsed = time-lastTime;//timer->getSeconds();
 		lastTime = time;
+		std::cout << "frame " << siFrame++ << " fps " << ((timeElapsed > 0) ? (1./timeElapsed) : 0) << std::endl;
 
 		//
 		mPWorld->step( timeElapsed );
 		std::cout << "ball at " << *(s.pActor) << std::endl;
 		s.update();
+		ball2.update();
+		for (size_t i=0; i<kNumBoxes; ++i)
+			boxes[i].update();
 
 		mGWorld->render( timeElapsed );
 	}
