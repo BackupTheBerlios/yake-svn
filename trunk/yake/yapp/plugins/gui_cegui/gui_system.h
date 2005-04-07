ac6131abe9cb1a4a6a0ec6fd0fb4d868161981d5
metax <metax@21facc31-32d9-0310-a21e-8743ab810763>
2005-04-07 16:12:54 +0000

    fixes

--- trunk/yake/yapp/plugins/gui_cegui/gui_system.h
+++ trunk/yake/yapp/plugins/gui_cegui/gui_system.h
@@ -1,15 +1,10 @@
-#ifndef _YAPP_GUI_SYSTEM_H_
-#define _YAPP_GUI_SYSTEM_H_
-
-// stl
-#include <typeinfo>
-// boost
-#include <boost/shared_ptr.hpp>
 #ifndef _YAPP_PLUGINS_GUI_CEGUI_GUI_SYSTEM_H_
 #define _YAPP_PLUGINS_GUI_CEGUI_GUI_SYSTEM_H_
 
 // yapp
 #include <yapp/gui/gui_system_base.h>
+#include <yapp/plugins/gui_cegui/controls.h>
+#include <yapp/plugins/gui_cegui/config.h>
 
 namespace yake
 {
@@ -18,16 +13,13 @@ namespace gui
 namespace cegui
 {
 
-struct YAPP_GUI_API gui_system
+struct YAPP_GUI_CEGUI_API gui_system
 	: yake::base::mpl::concrete_factory
 		<
 			gui_system_base,
-			implemented_widgets_concrete // todo: should be abstract_widgets, concrete_widgets or something
+			implemented_widgets
 		>
-{
-
-};
-
+{};
 
 } // namespace cegui
 } // namespace gui
