61477012e689026b2e967ae971bee66410a7db8b
metax <metax@21facc31-32d9-0310-a21e-8743ab810763>
2005-04-08 13:07:45 +0000

    moved

---
+++ trunk/yake/src/yapp/gui/widgets/static_text_base.cpp
@@ -0,0 +1,19 @@
+#include "static_text_base.h"
+#include <typeinfo>
+
+namespace yake
+{
+namespace gui
+{
+
+// defaults
+float static_text_base::defaults::size::height = 0.05f;
+float static_text_base::defaults::size::width = 0.2f;
+float static_text_base::defaults::position::x = 0.1f;
+float static_text_base::defaults::position::y = 0.1f;
+const char * static_text_base::defaults::text = typeid(static_text_base).name();
+
+GUI_IMPLEMENT_CLASS(static_text_base)
+
+} // namespace gui
+} // namespace yake
