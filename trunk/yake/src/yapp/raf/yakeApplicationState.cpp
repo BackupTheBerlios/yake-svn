ac60a086d585c631db728ae48228874d19f24f61
codeandroid <codeandroid@21facc31-32d9-0310-a21e-8743ab810763>
2005-07-27 22:24:54 +0000

    initial revision

---
+++ trunk/yake/src/yapp/raf/yakeApplicationState.cpp
@@ -0,0 +1,73 @@
+/*
+   ------------------------------------------------------------------------------------
+   This file is part of YAKE
+   Copyright (c) 2004 The YAKE Team
+   For the latest information visit http://www.yake.org 
+   ------------------------------------------------------------------------------------
+   This program is free software; you can redistribute it and/or modify it under
+   the terms of the GNU Lesser General Public License as published by the Free Software
+   Foundation; either version 2 of the License, or (at your option) any later
+   version.
+
+   This program is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+   FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public License along with
+   this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+   Place - Suite 330, Boston, MA 02111-1307, USA, or go to
+   http://www.gnu.org/copyleft/lesser.txt.
+   ------------------------------------------------------------------------------------
+   If you are interested in another license model contact the Yake Team via
+   E-Mail: team@yake.org.
+   For more information see the LICENSE file in the root directory of the
+   source code distribution.
+   ------------------------------------------------------------------------------------
+*/
+#include <yapp/raf/pch.h>
+#include <yapp/raf/yakeApplicationState.h>
+
+namespace yake {
+namespace raf {
+
+	//-----------------------------------------------------
+	// Class: ApplicationState
+	//-----------------------------------------------------
+	ApplicationState::ApplicationState(Application& owner) :
+		mOwner(owner)
+	{
+	}
+	ApplicationState::~ApplicationState()
+	{
+	}
+	Application& ApplicationState::getApp()
+	{
+		return mOwner;
+	}
+	const Application& ApplicationState::getApp() const
+	{
+		return mOwner;
+	}
+
+	//-----------------------------------------------------
+	// Class: MainState
+	//-----------------------------------------------------
+	MainState::MainState(Application& owner) :
+		ApplicationState(owner)
+	{
+	}
+	void MainState::onEnter()
+	{
+		std::cout << "MainState::onEnter()\n";
+	}
+	void MainState::onExit()
+	{
+		std::cout << "MainState::onExit()\n";
+	}
+	void MainState::onStep()
+	{
+		std::cout << "MainState::onStep()\n";
+	}
+
+} // namespace raf
+} // namespace yake
---
+++ trunk/yake/yapp/raf/yakeRtApplicationState.h
@@ -0,0 +1,85 @@
+/*
+   ------------------------------------------------------------------------------------
+   This file is part of YAKE
+   Copyright (c) 2004 The YAKE Team
+   For the latest information visit http://www.yake.org 
+   ------------------------------------------------------------------------------------
+   This program is free software; you can redistribute it and/or modify it under
+   the terms of the GNU Lesser General Public License as published by the Free Software
+   Foundation; either version 2 of the License, or (at your option) any later
+   version.
+
+   This program is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+   FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public License along with
+   this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+   Place - Suite 330, Boston, MA 02111-1307, USA, or go to
+   http://www.gnu.org/copyleft/lesser.txt.
+   ------------------------------------------------------------------------------------
+   If you are interested in another license model contact the Yake Team via
+   E-Mail: team@yake.org.
+   For more information see the LICENSE file in the root directory of the
+   source code distribution.
+   ------------------------------------------------------------------------------------
+*/
+#ifndef YAKE_RAF_RTAPPLICATIONSTATE_H
+#define YAKE_RAF_RTAPPLICATIONSTATE_H
+
+namespace yake {
+namespace raf {
+	class Application;
+	class MainState;
+
+	/** Typical realtime application main state */
+	class YAKE_RAF_API RtMainState : public MainState
+	{
+	public:
+		RtMainState(Application& owner) :
+			MainState(owner), 
+			mQuitRequested(false)
+		{}
+		void requestQuit();
+		bool quitRequested() const;
+	protected:
+		// main message handlers to override
+
+		virtual void onEnter();
+		virtual void onStep();
+		virtual void onExit();
+	private:
+		// setup functions
+
+		void createScene();
+		void destroyScene();
+		void createCameraViewportPair();
+
+	protected:
+		// message handlers to override by derived classes
+		virtual void onCreateScene() {}
+		virtual graphics::ICamera* onCreateDefaultCamera();
+		virtual graphics::IViewport* onCreateDefaultViewport();
+		virtual void onDestroyScene() {}
+		virtual void onFrame(const real timeElapsed) {}
+		virtual bool onQuitRequested() { return true; }
+
+		// other ...
+		graphics::IWorld* getGraphicalWorld();
+		audio::IWorld* getAudioWorld();
+
+		graphics::ICamera* getDefaultCamera();
+	private:
+		bool						mQuitRequested;
+		SharedPtr<graphics::IWorld>	mGWorld;
+		SharedPtr<audio::IWorld>	mAWorld;
+
+		typedef std::vector<SharedPtr<graphics::IViewport> > ViewportList;
+		ViewportList				mViewports;
+		graphics::ICamera*			mDefaultCamera;
+	};
+
+} // namespace raf
+} // namespace yake
+
+#endif
