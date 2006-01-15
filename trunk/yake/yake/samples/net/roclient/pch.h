787a9c499d75e98ffb7071d218c3e94c911ec83c
codeandroid <codeandroid@21facc31-32d9-0310-a21e-8743ab810763>
2006-01-15 21:42:37 +0000

    initial revision

---
+++ trunk/yake/yake/samples/net/roclient/pch.h
@@ -0,0 +1,7 @@
+#ifndef PCH_H
+#define PCH_H
+
+#include <boost/function.hpp>
+#include <boost/bind.hpp>
+
+#endif
---
+++ trunk/yake/yake/samples/net/roserver/pch.h
@@ -0,0 +1,11 @@
+#ifndef PCH_H
+#define PCH_H
+
+#include <boost/function.hpp>
+#include <boost/bind.hpp>
+#include <map>
+#include <vector>
+#include <deque>
+#include <boost/thread/mutex.hpp>
+
+#endif
