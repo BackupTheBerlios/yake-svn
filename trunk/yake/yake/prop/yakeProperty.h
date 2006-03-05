d0321da0712ae34f698c2c683798a0f729b95c72
codeandroid <codeandroid@21facc31-32d9-0310-a21e-8743ab810763>
2006-03-05 18:40:01 +0000

    initial revision

---
+++ trunk/yake/yake/prop/yakeProperty.h
@@ -0,0 +1,10 @@
+#ifndef YAKE_PROPERTY_H
+#define YAKE_PROPERTY_H
+
+#include "yake/prop/prerequisites.h"
+#include "yake/prop/prop_def.h"
+#include "yake/prop/prop.h"
+#include "yake/prop/prop_holder.h"
+#include "yake/prop/rtti_class.h"
+
+#endif
