e013a9117a6881f648381f7ca537f9b56c6f5f98
codeandroid <codeandroid@21facc31-32d9-0310-a21e-8743ab810763>
2006-03-12 18:56:19 +0000

    renamed namespace, next development state

--- trunk/yake/src/yake/plugins/entLua/entLua.cpp
+++ trunk/yake/src/yake/plugins/entLua/entLua.cpp
@@ -6,7 +6,7 @@
 #include <luabind/luabind.hpp>
 
 namespace yake {
-namespace object {
+namespace ent {
 	namespace detail {
 		void executeOfTable_2(scripting::IVM& vm, const std::string& tbl1name, const std::string& tbl2name, const std::string& fn)
 		{
@@ -48,44 +48,60 @@ namespace object {
 	LuaFsmObjectListener::LuaFsmObjectListener(scripting::IScriptingSystem& scriptingSystem) :
 		scriptingSystem_(scriptingSystem)
 	{
-		startScript_.reset( scriptingSystem_.createScriptFromFile("../o_test.lua") );
+		startScript_.reset( scriptingSystem_.createScriptFromFile("../../media/scripts/o_fsm_test.lua") );
 		YAKE_ASSERT( startScript_.get() );
 	}
 	void LuaFsmObjectListener::onInit(Object& obj)
 	{
-		//YAKE_ASSERT( obj.getFsmVM() );
-		obj.getFsmVM()->execute("print('initializing...')");
-		obj.getFsmVM()->execute( startScript_.get() );
+		Entity* ent = Entity::cast(&obj);
+		if (ent)
+		{
+			YAKE_ASSERT( ent->getFsmVM() );
+			ent->getFsmVM()->execute("print('initializing...')");
+			ent->getFsmVM()->execute( startScript_.get() );
 
-		//std::cout << "vm: exec start_main()\n";
-		//@todo make sure "main()" exists
-		//obj.getVM().execute("start_main()");
-
-		obj.getFsmVM()->execute("c = coroutine.create(main)\ncoroutine.resume(c,1)");
+			ent->getFsmVM()->execute("c = coroutine.create(main)\ncoroutine.resume(c,1)");
+		}
 	}
 	void LuaFsmObjectListener::onTick(Object& obj)
 	{
-		obj.getFsmVM()->execute("event.on_tick()");
-		obj.getFsmVM()->execute("state." + obj.getCurrentFsmState() + ".on_tick()");
+		Entity* ent = Entity::cast(&obj);
+		if (ent)
+		{
+			ent->getFsmVM()->execute("event.on_tick()");
+			ent->getFsmVM()->execute("state." + ent->getCurrentFsmState() + ".on_tick()");
+		}
 	}
 	void LuaFsmObjectListener::onFsmEventHandled(Object& obj, const object_fsm&, const object_fsm::event_type& evt)
 	{
-		if (evt == "spawn")
-			obj.getFsmVM()->execute("event.on_spawn()");
-		else if (evt == "die")
-			obj.getFsmVM()->execute("event.on_die()");
+		Entity* ent = Entity::cast(&obj);
+		if (ent)
+		{
+			if (evt == "spawn")
+				ent->getFsmVM()->execute("event.on_spawn()");
+			else if (evt == "die")
+				ent->getFsmVM()->execute("event.on_die()");
+		}
 	}
 	void LuaFsmObjectListener::onFsmEnterState(Object& obj,/*const std::string& fsmName, */const object_fsm&, const object_fsm::state_type& state)
 	{
-		scripting::IVM* vm = obj.getFsmVM();
-		YAKE_ASSERT( vm );
-		detail::executeOfTable_2(*vm,"state",state,"on_enter");
+		Entity* ent = Entity::cast(&obj);
+		if (ent)
+		{
+			scripting::IVM* vm = ent->getFsmVM();
+			YAKE_ASSERT( vm );
+			detail::executeOfTable_2(*vm,"state",state,"on_enter");
+		}
 	}
 	void LuaFsmObjectListener::onFsmExitState(Object& obj,/*const std::string& fsmName, */const object_fsm&, const object_fsm::state_type& state)
 	{
-		scripting::IVM* vm = obj.getFsmVM();
-		YAKE_ASSERT( vm );
-		detail::executeOfTable_2(*vm,"state",state,"on_exit");
+		Entity* ent = Entity::cast(&obj);
+		if (ent)
+		{
+			scripting::IVM* vm = ent->getFsmVM();
+			YAKE_ASSERT( vm );
+			detail::executeOfTable_2(*vm,"state",state,"on_exit");
+		}
 	}
 	LuaObjectManagerListener::LuaObjectManagerListener(scripting::IScriptingSystem& scriptingSystem) :
 		fsmListener_(scriptingSystem)
@@ -93,9 +109,13 @@ namespace object {
 	}
 	void LuaObjectManagerListener::onObjectCreated(Object* obj)
 	{
-		YAKE_ASSERT( obj );
-		obj->attachListener( &fsmListener_, "lua_listener", object_listener_mgr::kKeepOwnership );
+		Entity* ent = Entity::cast(obj);
+		if (ent)
+		{
+			YAKE_ASSERT( ent );
+			ent->attachListener( &fsmListener_, "lua_listener", ObjectListenerManager::kKeepOwnership );
+		}
 	}
-}
+} // namespace ent
 } // namespace yake
 
