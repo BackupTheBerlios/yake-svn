7ecdff1c7ee6668f8e12d7b61117376027fdefae
codeandroid <codeandroid@21facc31-32d9-0310-a21e-8743ab810763>
2006-03-12 18:49:52 +0000

    initial revision

---
+++ trunk/yake/documentation/api/docMainPage.h
@@ -0,0 +1,27 @@
+/** \mainpage "YAKE 0.5.0 API"
+
+!!! YAKE 0.5.0 API Documentation
+
+
+!! What is this?
+
+This is the API documentation for YAKE. It contains specifications for each class, method and variable which can be used in code you write.
+
+!! Where do I start?
+
+The API documentation is most useful as a reference to YAKE. It is not intended to be a guide which introduces you to developing software with YAKE.
+If you know your way around YAKE the reference documentation will become handy and you will want to make use of it.
+
+To get started with the concepts and basics of YAKE you should refer to the more discussive material in the manual and tutorials.
+You find these in the Wiki (http://www.yake.org/wiki). If you don't find what you search either in the Wiki or the API reference then
+you may want to try out the community features and get in contact with YAKE's developers and users. Information on forums, IRC and other ways of contact can be found at the YAKE website (http://www.yake.org).
+
+
+
+
+
+Copyright (C) The YAKE Team
+
+Permission is granted to make and distribute verbatim copies of this API documentation provided the copyright notice and this permission notice are preserved on all copies.
+
+*/
