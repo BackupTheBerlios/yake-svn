cb944c7833bfd73dfe3e03e2ca2b42a2636e547a
codeandroid <codeandroid@21facc31-32d9-0310-a21e-8743ab810763>
2006-03-15 20:02:26 +0000

    added license + comments

--- trunk/yake/yake/statemachine/fsm_core.h
+++ trunk/yake/yake/statemachine/fsm_core.h
@@ -1,3 +1,29 @@
+/*
+   ------------------------------------------------------------------------------------
+   This file is part of YAKE
+   Copyright (c) The YAKE Team
+   For the latest information visit http://www.yake.org 
+   ------------------------------------------------------------------------------------
+   This program is free software; you can redistribute it and/or modify it under
+   the terms of the GNU Lesser General Public License as published by the Free Software
+   Foundation; either version 2 of the License, or (at your option) any later
+   version.
+
+   This program is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+   FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public License along with
+   this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+   Place - Suite 330, Boston, MA 02111-1307, USA, or go to
+   http://www.gnu.org/copyleft/lesser.txt.
+   ------------------------------------------------------------------------------------
+   If you are interested in another license model contact the Yake Team via
+   E-Mail: team@yake.org.
+   For more information see the LICENSE file in the root directory of the
+   source code distribution.
+   ------------------------------------------------------------------------------------
+*/
 #ifndef FSM_CORE_H
 #define FSM_CORE_H
 
@@ -24,27 +50,39 @@
 
 namespace fsm {
 #if defined(FSM_USE_NULL_STATE)
+	/** Returns the state representing a 'null state' for a given type (as template parameter).
+		This function can be specialized for custom types. Specializations for common types are
+		provided (string, int ...).
+		See the documentation of machine<> for more information on null states.
+		@see machine<>
+	*/
 	template<typename state_type_>
 	const state_type_& get_null_state();
 
+	/** Returns whether a state is equal to the null state for this type.
+		This function can be specialized for custom types when necessary.
+	*/
 	template<typename state_type_>
 	inline bool is_state_null(const state_type_& state)
 	{
 		return (state == get_null_state<state_type_>());
 	}
 
+	/** Specialization for 'int'. */
 	template<>
 	inline const int& get_null_state()
 	{
 		static int ms_null = 0xffffffff;
 		return ms_null;
 	}
+	/** Specialization for 'unsigned char'. */
 	template<>
 	inline const unsigned char& get_null_state()
 	{
 		static unsigned char ms_null = 0xff;
 		return ms_null;
 	}
+	/** Specialization for 'std::string'. */
 	template<>
 	inline const std::string& get_null_state()
 	{
@@ -52,12 +90,14 @@ namespace fsm {
 		return ms_null;
 	}
 #endif
+	/** Generic operator object with a single parameter that does nothing (as the name implies). */
 	template<typename T1>
 	struct do_nothing_on_one
 	{
 		void operator()(const T1&)
 		{}
 	};
+	/** Generic operator object with two parameters that does nothing (as the name implies). */
 	template<typename T1, typename T2>
 	struct do_nothing_on_two
 	{
@@ -65,39 +105,78 @@ namespace fsm {
 		{}
 	};
 
+	/** Basic generic finite state machine. State and event types can be configured per machine type at compile-time.
+		Machine instances are configured at run-time (i.e. states and transitions are added at run-time).
+		@Remarks boost::statechart looks like a good candidate for a compile-time only state machine.
+		@todo add findPathToState() which finds a transition path from the current to the specified destination state
+				and enables the client to execute processEvent() for each event in the path.
+		@see oomachine
+	*/
 	template<typename _state_type, typename _event_type>
 	struct machine
 	{
 		// types
+
+		/** Type of this machine's states. */
 		typedef _state_type state_type;
+
+		/** Type of this machine's events. */
 		typedef _event_type event_type;
+
+		/** Type to self. */
 		typedef machine machine_type;
 
+		/** Do nothing operation - used as default in processEvent(). */
 		typedef do_nothing_on_one<state_type> do_nothing_state_op;
+
+		/** Do nothing operation - used as default in processEvent(). */
 		typedef do_nothing_on_two<machine_type,state_type> do_nothing_op;
 
 		// construction
 #if defined(FSM_USE_NULL_STATE)
+		/** Constructs a machine. Sets the current state to the state type's null state. */
 		machine() : 
 			curr_(get_null_state<state_type>()) 
 #else
+		/** Constructs a machine. */
 		machine()
 #endif
 		{}
 
 		// operations
+
+		/** Add a state.
+			@Remarks Client code has to take care that the machine is in a valid state when events are processed.
+		*/
 		void addState(const state_type&);
 		//? void addEvent(const event_type);
+
+		/** Add a transition.
+			@Remarks Client code has to take care that the machine is in a valid state when events are processed.
+		*/
 		void addTransition(const state_type&, const event_type&, const state_type&);
+
+		/** Explicitely set the current state.
+			A call to this function can be useful if the initial state has to be set explicitely. By default (if null
+			states are enabled) the initial state is the null state and a transition from a null state to a custom
+			state added with addState() has to be specified by the client code.
+		*/
 		void setState(const state_type&);
 
+		/** Clears the machine, i.e. removes all states and transitions.
+		*/
 		void clear();
 
+		/** Returns the number of states that have been added with addState(). */
 		size_t numStates() const;
+		/** Returns the number of outgoing transitions for a specific state. */
 		size_t numOffgoingTransitions(const state_type&) const; // off-going transitions for source state
+
+		/** Returns a reference to the current state. */
 		const state_type& current() const;
 
 #ifdef _DEBUG
+		/** DEBUG-ONLY function for dumping the machine contents to a std::ostream object. */
 		void dump(std::ostream& out) const
 		{
 			out << "machine<>::dump()\n";
@@ -114,14 +193,24 @@ namespace fsm {
 		}
 #endif
 
+		/** This method validates the machine by making sure that states and transitions are properly connected, i.e.
+			it finds unconnected states and invalid transitions.
+			@todo implement!
+		*/
 		std::pair<bool,std::string> validate();
 
+		/** Processes the given event thereby triggering a transition from the current state to the transition's target state.
+			If no transition can be found for the combination of currently active state and given event, the method returns without doing anything.
+		*/
 		void processEvent(const event_type& evt)
 		{
 			do_nothing_op do_nothing;
 			processEventCb(evt, do_nothing, do_nothing);
 		}
 
+		/** Same as processEvent but this method allows to provide custom callbacks for 'enter state' and 'exit state' calls.
+			@copydoc processEvent
+		*/
 		template<typename enter_state_op_t, typename exit_state_op_t>
 		void processEventCb(const event_type& evt, enter_state_op_t& enterOp, exit_state_op_t& exitOp)
 		{
