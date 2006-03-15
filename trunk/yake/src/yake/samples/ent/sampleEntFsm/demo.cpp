d7aa5a1dd58a59633f16d087e5c10f0f41732c51
codeandroid <codeandroid@21facc31-32d9-0310-a21e-8743ab810763>
2006-03-15 20:20:24 +0000

    initial revision

--- trunk/yake/src/yake/samples/ent/sampleEntFsm/demo.cpp
+++ trunk/yake/src/yake/samples/ent/sampleEntFsm/demo.cpp
@@ -58,15 +58,127 @@ namespace exapp {
 		scripting::IScriptingSystem&	scriptingSys_;
 	};
 
-	struct SimpleGraphicsCo : public ent::Co
+	struct SimpleGraphicsCo : public ent::Component
 	{
 		DECL_CO(SimpleGraphicsCo,"SimpleGraphicsCo")
 	};
-	IMPL_CO_1(SimpleGraphicsCo,"Co")
+	IMPL_CO_1(SimpleGraphicsCo,"Component")
 } // namespace exapp
+#ifdef OOSTATE
+struct oostate 
+{
+	virtual ~oostate() {}
+	virtual void onEnter() {}
+	virtual void onExit() {}
+};
+template<typename _state_class_type /*= oostate */, typename _event_type>
+struct oomachine
+{
+	typedef _event_type event_type;
+	typedef _state_class_type* state_type;
 
+	oomachine() :
+		fnEnter_( boost::bind(&oomachine::onEnter,this,_1,_2) ),
+		fnExit_( boost::bind(&oomachine::onExit,this,_1,_2) )
+	{
+	}
+
+	void addState(const state_type);
+	void addTransition(const state_type, const event_type&, const state_type);
+	void setState(const state_type);
+	void processEvent(const event_type& evt);
+	const state_type& getCurrentFsmState() const
+	{ return m_.current(); }
+private:
+	typedef fsm::machine<state_type,event_type> machine_type;
+	void onEnter(machine_type&,const state_type&);
+	void onExit(machine_type&,const state_type&);
+private:
+	machine_type			machine_;
+	typedef boost::function<void(machine_type&,const state_type&)> CbFn;
+	CbFn						fnEnter_;
+	CbFn						fnExit_;
+};
+template<typename _state_class_type, typename _event_type>
+void oomachine<_state_class_type,_event_type>::onEnter(machine_type&,const state_type& state)
+{
+	YAKE_ASSERT( state );
+	state->onEnter();
+}
+template<typename _state_class_type, typename _event_type>
+void oomachine<_state_class_type,_event_type>::onExit(machine_type&,const state_type& state)
+{
+	YAKE_ASSERT( state );
+	state->onExit();
+}
+template<typename _state_class_type, typename _event_type>
+void oomachine<_state_class_type,_event_type>::addState(const state_type s)
+{
+	machine_.addState(s);
+}
+template<typename _state_class_type, typename _event_type>
+void oomachine<_state_class_type,_event_type>::addTransition(const state_type from, const event_type& ev, const state_type to)
+{
+	machine_.addTransition( from, ev, to );
+}
+template<typename _state_class_type, typename _event_type>
+void oomachine<_state_class_type,_event_type>::setState(const state_type s)
+{
+	machine_.setState( s );
+}
+template<typename _state_class_type, typename _event_type>
+void oomachine<_state_class_type,_event_type>::processEvent(const event_type& evt)
+{
+	YAKE_ASSERT( !fnEnter_.empty() );
+	machine_.processEventCb(evt, fnEnter_, fnExit_);
+}
+struct test_state_base
+{
+	virtual ~test_state_base() {}
+	virtual void onEnter() {}
+	virtual void onExit() {}
+};
+typedef test_state_base* test_state_base_ptr;
+namespace fsm {
+	template<>
+	inline const test_state_base_ptr& get_null_state()
+	{ 
+		static test_state_base* ms_null = 0;
+		return ms_null; 
+	}
+}
+struct test_state : public test_state_base
+{
+	test_state(const std::string& name) : name_(name)
+	{}
+	virtual void onEnter()
+	{
+		std::cout << name_ << ".onEnter()\n";
+	}
+	virtual void onExit()
+	{
+		std::cout << name_ << ".onExit()\n";
+	}
+private:
+	std::string	name_;
+};
+void test()
+{
+	typedef oomachine<test_state_base,std::string> machine_t;
+	test_state_base* first = new test_state("first");
+	test_state_base* second = new test_state("second");
+
+	machine_t machine;
+	machine.addState( first );
+	machine.addState( second );
+	machine.setState( first );
+	machine.addTransition( first, "go", second );
+	machine.processEvent("go");
+}
+#endif
 int main(int argc, char* argv[])
 {
+	//test();
 
 	// Create the scripting system we want to use (Lua).
 	SharedPtr<scripting::IScriptingSystem> luaSys = templates::create<scripting::IScriptingSystem>("lua");
@@ -110,16 +222,16 @@ int main(int argc, char* argv[])
 	/*
 	RttiClassMgr clsMgr;
 	clsMgr.insert( ent::Object::cls() );
-	clsMgr.insert( ent::Co::cls() );
+	clsMgr.insert( ent::Component::cls() );
 	exapp::SimpleGraphicsCo::cls()->add( PropDef("position","position",Vector3(0,0,0)) );
 	clsMgr.insert( exapp::SimpleGraphicsCo::cls() );
 
-	e->addCo("gfx",new exapp::SimpleGraphicsCo());
-	e->getCo("gfx")->init();
-	if (!e->getCo("gfx")->setValue("position",Vector3(10,0,0)))
+	e->addComponent("gfx",new exapp::SimpleGraphicsCo());
+	e->getComponent("gfx")->init();
+	if (!e->getComponent("gfx")->setValue("position",Vector3(10,0,0)))
 		std::cerr << "could not set position\n";
 	Vector3 pos(0,0,0);
-	if (e->getCo("gfx")->getValue<Vector3>("position",pos))
+	if (e->getComponent("gfx")->getValue<Vector3>("position",pos))
 		std::cout << "position=" << pos << "\n";
 	else
 		std::cerr << "could not get position\n";
