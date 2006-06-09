75f2e9bdd35b4c53769160f517f743332cf985ce
codeandroid <codeandroid@21facc31-32d9-0310-a21e-8743ab810763>
2006-06-09 17:00:57 +0000

    cleanup

--- trunk/yake/src/yake/samples/base/registry/yakeUnitTest.cpp
+++ trunk/yake/src/yake/samples/base/registry/yakeUnitTest.cpp
@@ -1,96 +1,68 @@
-//****************************************************************************
-//**
-//**    UNITTEST.CPP
-//**    Unit test
-//**
-//****************************************************************************
-
-//============================================================================
-//    IMPLEMENTATION HEADERS
-//============================================================================
 // Yake
 #include <yake/base/yake.h>
 #include <yake/base/templates/yakeRegistry.h>
 
-//============================================================================
-//    INTERFACE STRUCTURES / UTILITY CLASSES
-//============================================================================
-using namespace yake::base;
-using namespace yake::base::templates;
+using namespace yake;
+using namespace yake::templates;
 
-namespace yake
-{
-namespace testsuite
-{
+namespace yake {
+namespace testsuite {
 	
-struct INetworkDriver
-{
-	// Destructor.
-	virtual ~INetworkDriver() {}
-
-	// Resolve host.
-	virtual const String resolveHost() const = 0;
-
-YAKE_DECLARE_REGISTRY_0( INetworkDriver, String )
-};
-
-YAKE_IMPLEMENT_REGISTRY( INetworkDriver )
-
-
-namespace win32
-{
-
-class NetworkDriver : public INetworkDriver
-{		
-YAKE_DECLARE_CLASS( yake::testsuite::win32::NetworkDriver )
-// Class
-public:
-	// Destructor.
-	virtual ~NetworkDriver() {}
-
-// Methods
-public:
-	// Resolve host.
-	const String resolveHost() const
+	struct INetworkDriver
 	{
-		return String( "Hello, its me - the win32 implementation :)" );
-	}
+		YAKE_DECLARE_REGISTRY_0( INetworkDriver, String )
 
-YAKE_DECLARE_CONCRETE( NetworkDriver, "win32" )
-};
+		// Destructor.
+		virtual ~INetworkDriver() {}
 
-YAKE_REGISTER_CONCRETE( NetworkDriver )
+		// Resolve host.
+		virtual const String resolveHost() const = 0;
+	};
 
-}
-namespace Linux
-{
+	YAKE_IMPLEMENT_REGISTRY( INetworkDriver )
 
-class NetworkDriver : public INetworkDriver
-{
 
-YAKE_DECLARE_CLASS( yake::testsuite::linux::NetworkDriver )
-// Class
-public:
-	// Destructor.
-	virtual ~NetworkDriver() {}
+namespace win32 {
 
-// Methods
-public:
-	// Resolve host.
-	const String resolveHost() const
+	class NetworkDriver : public INetworkDriver
+	{		
+	public:
+		YAKE_DECLARE_CONCRETE( NetworkDriver, "win32" )
+
+		virtual ~NetworkDriver() {}
+
+		virtual const String resolveHost() const
+		{
+			return String( "Hello, its me - the win32 implementation :)" );
+		}
+	};
+
+	YAKE_REGISTER_CONCRETE( NetworkDriver )
+
+} // namespace win32
+
+namespace Linux {
+
+	class NetworkDriver : public INetworkDriver
 	{
-		return String( "Hello, its me - the Linux implementation ;)" );
-	}
+	public:
+		YAKE_DECLARE_CONCRETE( NetworkDriver, "linux" )
 
-YAKE_DECLARE_CONCRETE( NetworkDriver, "linux" )
-};
+		virtual ~NetworkDriver() {}
 
-YAKE_REGISTER_CONCRETE( NetworkDriver )
 
-} // linux
+		virtual const String resolveHost() const
+		{
+			return String( "Hello, its me - the Linux implementation ;)" );
+		}
+	};
 
-} // testsuite
-} // yake
+	YAKE_REGISTER_CONCRETE( NetworkDriver )
+
+} // namespace linux
+
+} // namespace testsuite
+} // namespace yake
 
 //============================================================================
 //    IMPLEMENTATION FUNCTIONS
@@ -98,12 +70,19 @@ YAKE_REGISTER_CONCRETE( NetworkDriver )
 int main()
 {
 	using namespace yake;
-	using namespace yake::base;
 	using namespace yake::testsuite;
 
+	// Create a 'default' driver. (Can be 'Win32' or 'linux', whatever is registered first (or at all)).
 	SharedPtr< INetworkDriver > pNetDriver = create_default< INetworkDriver >();
-
 	std::cout << pNetDriver->resolveHost();
+
+	// Explicitely create the 'Win32' driver.
+	SharedPtr< INetworkDriver > pWin32NetDriver = create< INetworkDriver >("win32");
+	std::cout << pWin32NetDriver->resolveHost();
+
+	// Explicitely create the 'Linux' driver.
+	SharedPtr< INetworkDriver > pLinuxNetDriver = create< INetworkDriver >("linux");
+	std::cout << pLinuxNetDriver->resolveHost();
 
 	// Wait for a keystroke (in debug builds only).
 #if defined( YAKE_DEBUG_BUILD )
